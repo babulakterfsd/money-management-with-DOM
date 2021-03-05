92716921477b8a1d2cb35c5449c79df735aa5099
babulakterfsd <babulakterfsd@gmail.com>
2021-03-06 00:11:57 +0600

    c

--- js/main.js
+++ js/main.js
@@ -1,2 +1,10 @@
 'use strict';
 
+// targeting all the essential elements
+let totalIncome = document.getElementById(`totalincomeinput`).value;
+let totalExpense = document.getElementById(`totalexpenseinput`).value;
+let currentBalance = document.getElementById(`currentbalanceinput`);
+
+//current balance 
+let currentBalanceValue = totalIncome - totalExpense;
+currentBalance.value = currentBalanceValue;

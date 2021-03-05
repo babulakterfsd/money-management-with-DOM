4f4d517602400f02dc99872d9567bfff71448cbf
babulakterfsd <babulakterfsd@gmail.com>
2021-03-05 11:06:24 +0600

    c

--- js/main.js
+++ js/main.js
@@ -1,19 +1,2 @@
-// code for preloader starts here
-document.getElementsByTagName('main')[0].style.display = 'none';
- document.querySelector('.loader').style.display = 'none';
- 
- window.addEventListener('load', function () {
-    
-     document.querySelector('.loader').style.display = 'block';
- 
-     setTimeout(function () {
-         document.getElementsByTagName('main')[0].style.display = 'block';
-         document.querySelector('.loader').style.display = 'none';
- 
-     }, 2550);
- 
- })
-
-
- // code for body starts from here 
+'use strict';
 

33c442beabc35a95ffdb9b209148f50cf6946ccd
babulakterfsd <babulakterfsd@gmail.com>
2021-03-10 21:30:31 +0600

    logic

--- js/main.js
+++ js/main.js
@@ -1,10 +1,53 @@
 'use strict';
 
-// targeting all the essential elements
-let totalIncome = document.getElementById(`totalincomeinput`).value;
-let totalExpense = document.getElementById(`totalexpenseinput`).value;
-let currentBalance = document.getElementById(`currentbalanceinput`);
+// cuurent balance
+let currentbalance = document.getElementById(`currentbalance`);
+let currentamount = Number(currentbalance.innerText)
 
-//current balance 
-let currentBalanceValue = totalIncome - totalExpense;
-currentBalance.value = currentBalanceValue;
+//add income
+let incomevalue;
+let totalincome = document.getElementById(`totalincome`);
+let addincomeform = document.getElementById(`addincomeform`);
+let sourceofincome = document.getElementById(`sourceofincome`);
+let amountofincome = document.getElementById(`amountofincome`);
+amountofincome.addEventListener(`keyup`,function(e) {
+    incomevalue = Number(e.target.value);
+})
+
+addincomeform.addEventListener(`submit`,function(e) {
+    e.preventDefault();
+    if(incomevalue < 0) {
+        alert(`Income can't be negative`)
+    } else {
+        let previousamount = Number(totalincome.innerText);
+        totalincome.innerText = previousamount + incomevalue;
+         
+        let totalamount = Number(totalincome.innerText);
+        currentamount += totalamount;
+        console.log(`total income: ${totalamount} is a ${typeof totalamount}`);
+        console.log(`current amount: ${currentamount} is a ${typeof currentamount}`);
+        currentbalance.innerText = currentamount;
+    }
+    e.target.reset()
+})
+
+//subtract expense
+let totalspent = document.getElementById(`totalspent`);
+let expenseform = document.getElementById(`subexpenseform`);
+let spentamount = document.getElementById(`subexpenseamountinput`);
+let spend;
+spentamount.addEventListener(`keyup`,function(e) {
+    spend = Number(e.target.value);
+})
+expenseform.addEventListener(`submit`,function(e) {
+    e.preventDefault();
+    if(spend <= 0) {
+        alert(`Value equal or smaller than 0 cam't be a spent value`)
+    } else {
+        let previouspent = Number(totalspent.innerText);
+        totalspent.innerText = previouspent + spend;
+        
+        currentbalance.innerText = totalincome.innerText - totalspent.innerText
+    }
+    e.target.reset();
+})

ac8da73d1a9bd3a51d4c1d78fc9fffa21438f506
babulakterfsd <babulakterfsd@gmail.com>
2021-03-11 12:29:20 +0600

    added

--- js/main.js
+++ js/main.js
@@ -6,18 +6,24 @@ let currentamount = Number(currentbalance.innerText)
 
 //add income
 let incomevalue;
+let incomesource;
 let totalincome = document.getElementById(`totalincome`);
 let addincomeform = document.getElementById(`addincomeform`);
 let sourceofincome = document.getElementById(`sourceofincome`);
 let amountofincome = document.getElementById(`amountofincome`);
+
+sourceofincome.addEventListener(`keyup`,function(e) {
+    incomesource = e.target.value;
+})
+
 amountofincome.addEventListener(`keyup`,function(e) {
     incomevalue = Number(e.target.value);
 })
 
 addincomeform.addEventListener(`submit`,function(e) {
     e.preventDefault();
-    if(incomevalue < 0) {
-        alert(`Income can't be negative`)
+    if(incomevalue <= 0 || incomevalue == undefined || incomesource == undefined ) {
+        alert(`Income can't be negative or empty or source is empty`)
     } else {
         let previousvalueoftotal = +totalincome.innerText;
         let currentBalance = +currentbalance.innerText;
@@ -25,6 +31,7 @@ addincomeform.addEventListener(`submit`,function(e) {
         totalincome.innerText = previousvalueoftotal + incomevalue;
         currentbalance.innerText = currentBalance + incomevalue;
     }
+    createIncomeLi()
     e.target.reset()
 })
 
@@ -32,19 +39,54 @@ addincomeform.addEventListener(`submit`,function(e) {
 let totalspent = document.getElementById(`totalspent`);
 let expenseform = document.getElementById(`subexpenseform`);
 let spentamount = document.getElementById(`subexpenseamountinput`);
+let spendsource = document.getElementById(`subexpensesrcinput`);
+let spent;
+spendsource.addEventListener(`keyup`, function(e) {
+     spent = e.target.value;
+})
 let spend;
 spentamount.addEventListener(`keyup`,function(e) {
     spend = Number(e.target.value);
 })
 expenseform.addEventListener(`submit`,function(e) {
     e.preventDefault();
-    if(spend <= 0) {
-        alert(`Value equal or smaller than 0 cam't be a spent value`)
+    if(spend <= 0 || spend == undefined || spent == undefined) {
+        alert(`spent value can't be 0 or undefined or spent source is undefined`)
     } else {
         let previouspent = Number(totalspent.innerText);
         totalspent.innerText = previouspent + spend;
         
         currentbalance.innerText = totalincome.innerText - totalspent.innerText
     }
+    createSpendLi()
     e.target.reset();
 })
+
+
+function createIncomeLi() {
+    let ul = document.getElementById(`myul`);
+    let li = document.createElement(`li`)
+    li.classList.add(`list-group-item`,`d-flex`,`justify-content-center`,`justify-content-sm-between`, `align-items-center`);
+    li.style.borderLeft = `10px solid green`;
+    li.innerHTML = 
+    `<span class="text-center font-weight-bold">${incomesource}</span>
+    <span class="text-center font-weight-bold">${incomevalue}</span>
+    <div class="imgcontainer bg-white p-1">
+      <img src="./images/delete.png" alt="delete" title="delete item" style="height: 25px; min-width: 25px; cursor: pointer;" class="img-fluid">
+    </div>`;
+    ul.appendChild(li)
+}
+
+function createSpendLi() {
+    let ul = document.getElementById(`myul`);
+    let li = document.createElement(`li`)
+    li.classList.add(`list-group-item`,`d-flex`,`justify-content-center`,`justify-content-sm-between`, `align-items-center`);
+    li.style.borderLeft = `10px solid red`;
+    li.innerHTML = 
+    `<span class="text-center font-weight-bold">${spent}</span>
+    <span class="text-center font-weight-bold">${spend}</span>
+    <div class="imgcontainer bg-white p-1">
+      <img src="./images/delete.png" alt="delete" title="delete item" style="height: 25px; min-width: 25px; cursor: pointer;" class="img-fluid">
+    </div>`;
+    ul.appendChild(li)
+}
